22d5c1ca53fdf8633440700a50a7f6c3f05e610f
Guilherme Silva <63061235+guilhermesilva1000@users.noreply.github.com>
2024-09-16 00:53:40 -0300

    fix: api url

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { store } from "./store";
-// import i18n from "./i18n";
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import {
   IoCloseSharp,
@@ -35,13 +34,12 @@ addIcons(
   CoOptions
 );
 
-axios.defaults.baseURL = "http://127.0.0.1:8080/api/v1";
-// axios.defaults.baseURL = "https://aprendiztech-api.onrender.com/api/v1";
+// axios.defaults.baseURL = "http://127.0.0.1:8080/api/v1";
+axios.defaults.baseURL = "https://aprendiztech-api.onrender.com/api/v1";
 
 const app = createApp(App);
 
 app.use(router);
-// app.use(i18n);
 app.use(store);
 app.component("v-icon", OhVueIcon);
 app.config.globalProperties.$axios = axios;
