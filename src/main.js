0303a4b1d5a2b0a4e32629086049aac8b8501624
Guilherme Silva <63061235+guilhermesilva1000@users.noreply.github.com>
2024-09-20 14:54:14 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -47,8 +47,8 @@ addIcons(
   HiSolidSearch
 );
 
-axios.defaults.baseURL = "http://127.0.0.1:8080/api/v1";
-// axios.defaults.baseURL = "https://aprendiztech-api.onrender.com/api/v1";
+// axios.defaults.baseURL = "http://127.0.0.1:8080/api/v1";
+axios.defaults.baseURL = "https://aprendiztech-api.onrender.com/api/v1";
 
 const app = createApp(App);
 
